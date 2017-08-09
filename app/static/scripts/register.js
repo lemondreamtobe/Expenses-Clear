83341fda5f9083026f3d269762d405c1039298dc
lemondreamtobe <897418436@qq.com>
2017-08-09 09:25:46 +0800

    注册

--- app/static/scripts/register.js
+++ app/static/scripts/register.js
@@ -5,4 +5,29 @@ $(function () {
     $('#cancel').on('click', function () {
         parent.closeDia();
     });
-})
+    $('#register').on('click', function () {
+        var params = {};
+        params.username = $('#user').val();
+        params.card = $('#card').val();
+        params.cash = $('#cash').val();
+        params.password = $('#key').val();
+        params.yct = $('#yct').val();
+        $.ajax({
+            url: '/register',
+            type: "post",
+            contentType: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(params),
+            cache: false,
+            error: function(evt) {
+                if (evt.status === 200 || evt.statusText === 'OK') {
+                    alert('注册并未成功！请通知管理员！')
+                }
+            },
+
+            success: function(ret) {
+
+            }
+        })
+    })
+});
